f0af05c988cabe1e9cdba20016df89dfc196138c
Andrea-Arguello <arg17801@uvg.edu.gt>
2020-08-18 00:02:48 -0600

    Added assets, added introduction

--- DataExploration.R
+++ DataExploration.R
@@ -5,3 +5,8 @@ rd_merge <- merge(rd,rd_tags)
 library(ggplot2)
 
 ggplot(rd_merge, aes(x=diagnosis_n,fill=diagnosis_n)) + geom_histogram(stat="count") + ylab(element_blank()) + xlab("Diagnosis") + ggtitle("Histogram of diagnosis in train dataset") + theme(legend.position = "none")
+
+rd_test <- read.csv("./csvs/test.csv")
+rd_test_m <- merge(rd_test, rd_tags)
+
+prop.table(table(rd_merge$diagnosis_n))
